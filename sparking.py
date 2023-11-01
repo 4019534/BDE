b8634839a99ed8856dc9ed48581a417dd43b1e82
4019534 <144572947+4019534@users.noreply.github.com>
2023-11-01 19:00:52 +0200

    Update sparking.py

--- sparking.py
+++ sparking.py
@@ -2,52 +2,30 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import when
 
 # Initialize a Spark session
-spark = SparkSession.builder.appName("CSV Merge").getOrCreate()
+spark = SparkSession.builder.appName("Filter CSV").getOrCreate()
 
-# Read the first CSV file (file1) into a DataFrame
-file1_df = spark.read.csv("hdfs://172.19.0.2:9000/hotels_latlong.csv", header=True, inferSchema=True)
-# Read the second CSV file (file2) into a DataFrame
-file2_df = spark.read.csv("hdfs://172.19.0.2:9000/improved_restaurants.csv", header=True, inferSchema=True)
+#read in the csv files 
+hotels = spark.read.csv("hdfs://172.19.0.2:9000/hotels_latlong.csv", header=True, inferSchema=True)
+rests = spark.read.csv("hdfs://172.19.0.2:9000/improved_restaurants.csv", header=True, inferSchema=True)
 
-# Perform a left join based on the 'address' column
-joinDF = file1_df.join(file2_df, file1_df['address'] == file2_df['address_x'], 'left')
+#configure columns using left join to merge address and address_x 
+joinDF = hotels.join(rests, hotels['address'] == rests['address_x'], 'left')
 
-# Add a new column 'rests' with restnames where available
+#Add rests columns for restaurants
 joinDF = joinDF.withColumn("rests", when(joinDF['restname'].isNotNull(), joinDF['restname']).otherwise(''))
 
-# Select the desired columns from the resulting DataFrame
-hotels_rests = joinDF.select(
-    "name",
-    "url",
-    "address",
-    "airport",
-    "distance",
-    "unit",
-    "rating",
-    "lat",
-    "long",
-    "rests"
-)
-
-# Save the updated DataFrame to a new CSV file or overwrite the original
-hotels_rests.write.csv("hotels_rests.csv", header=True, mode="overwrite")
-
-# Stop the Spark session
-
-# Initialize a Spark session
-spark = SparkSession.builder.appName("CSV Merge").getOrCreate()
-
-# Read the CSV file (file1_updated) into a DataFrame
-file1_updated_df = spark.read.csv("hotels_rests.csv", header=True, inferSchema=True)
-
-# Filter the DataFrame to exclude rows where 'rests' is empty
-filtered_df = file1_updated_df.filter(file1_updated_df['rests'] != "")
-filtered_df = filtered_df.filter(filtered_df["rating"] == "4.0")
-# Save the filtered DataFrame to a new CSV file
-filtered_df.write.csv("hdfs://172.19.0.2:9000/onlyhotels_rests.csv", header=True, mode="overwrite")
-
-filtered_df.show(10)
+#Include all relevant columns for the final csv
+hotels_rests = joinDF.select("name","url","address","airport","distance","unit","rating","lat","long","rests")
 
 
+#look only for hotels with restaurants
+fourStarhotels = hotels_rests.filter(hotels_rests['rests'] != "")
+#look only for hotels that are 4 star with restaurants
+fourStarhotels = fourStarhotels.filter(fourStarhotels["rating"] == "4.0")
+
+#save thse hotels in a new csv file and write it to hdfs
+fourStarhotels.write.csv("hdfs://172.19.0.2:9000/onlyhotels_rests.csv", header=True, mode="overwrite")
+#display 10 rows on screen when using spark-submit
+fourStarhotels.show(10)
 # Stop the Spark session
 spark.stop()
